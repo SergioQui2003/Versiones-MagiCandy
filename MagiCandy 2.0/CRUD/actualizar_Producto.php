4ab1ff7f9c0a8a51dc57be34fd319921715e2c2b
Niyiret28 <124094608+Niyiret28@users.noreply.github.com>
2023-09-20 20:54:17 -0500

    Update actualizar_Producto.php

--- MagiCandy 2.0/CRUD/actualizar_Producto.php
+++ MagiCandy 2.0/CRUD/actualizar_Producto.php
@@ -7,7 +7,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $cantidad = $_POST['cantidad'];
 
     // Realizar la actualización en la base de datos o almacenamiento
-
+    //
     // Redirigir de nuevo a la página principal
     header('Location: ../DASHPAGS/RegistraProducto.php');
     exit;
